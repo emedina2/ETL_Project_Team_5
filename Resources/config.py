1f01a5e2629687e50212c364ae85ad135dfed7f2
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-03-24 19:58:02 -0500

    Added csv of df

--- Resources/config.py
+++ Resources/config.py
@@ -1,2 +1,2 @@
 omdb_key = "a2083d5d"
-rds_connection_string = "postgres:donotcomeforme$$$@localhost:5432/Movie_ETL"
+postgres_connection_string = "postgres:donotcomeforme$$$@localhost:5432/Movie_ETL"
